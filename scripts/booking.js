8a2d438eb99940f119c74e09de3599ad79ad826e
asettar <achrafsettar8@gmail.com>
2025-11-12 17:10:53 +0100

    add add dynamic form update based on number of traveleres

--- scripts/booking.js
+++ scripts/booking.js
@@ -20,10 +20,23 @@ async function loadAccomodationData() {
 
 loadAccomodationData();
 
-let userDestination = document.getElementById('user-destination');
-let accomodationsContainer = document.getElementById('accomodations-container');
-console.log(userDestination);
-console.log(accomodationsContainer);
+const userDestination = document.getElementById('user-destination');
+const accomodationsContainer = document.getElementById('accomodations-container');
+const soloTraveler = document.getElementById('solo-traveler');
+const coupleTraveler = document.getElementById('couple-traveler');
+const groupTraveler = document.getElementById('group-traveler');
+const addPassengerBtn = document.getElementById('add-passenger');
+const submitBtn = document.getElementById('submit-btn');
+const informationForm = document.querySelector('.info-form');
+const infoFormContainer = document.getElementById('info-form-container');  
+
+// console.log(userDestination);
+// console.log(accomodationsContainer);
+console.log(soloTraveler);
+console.log(coupleTraveler);
+console.log(groupTraveler);
+console.log(informationForm);
+console.log(infoFormContainer);
 
 function    isAvailbaleDestination(accomdationData, destination) {
     const available = accomdationData["availableOn"];
@@ -49,6 +62,7 @@ function    checkUserDestination() {
     // update accomodations based on the picked destination
     let validAccomodations = [];  // availble for the current destination 
     const currentDestination = userDestination.value.toLowerCase();
+    console.log(currentDestination);
     accomodationsContainer.innerHTML = "";
     // console.log(typeof accomodationsData);
     // console.log(accomodationsData);
@@ -59,5 +73,49 @@ function    checkUserDestination() {
     }
 }
 
-// destination 
+
+function    addNewForm() {
+    const newForm = informationForm.cloneNode(true);
+    infoFormContainer.appendChild(newForm);
+}
+
+// to adjust num of forms based on selected option
+function    checkForms(need) {
+    console.log("checked", need);
+
+    let currentForms = infoFormContainer.querySelectorAll('.info-form');
+    currentFormsCount = currentForms.length;
+    console.log(currentFormsCount);
+    console.log(currentForms);
+    // if need to add
+    for (let i = currentFormsCount; i < need; i++)
+        addNewForm();
+
+    // if need to delete
+    while (currentFormsCount > need) {
+        currentForms[currentFormsCount - 1].remove();
+        currentFormsCount--;
+    }
+
+    if (need === 3) {
+        // add possibility to select add passenger btn
+        addPassengerBtn.classList.add('btn-primary');
+    }
+    else  addPassengerBtn.classList.remove('btn-primary');
+}
+
+// events
+
 userDestination.addEventListener('change', checkUserDestination);
+soloTraveler.addEventListener('change', () => {checkForms(1)});
+coupleTraveler.addEventListener('change', () => {checkForms(2)});
+groupTraveler.addEventListener('change', () => {checkForms(3)});
+addPassengerBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (groupTraveler.checked) addNewForm();
+    }
+);
+submitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log("submit clicked");
+})
